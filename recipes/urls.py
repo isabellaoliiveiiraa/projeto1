90ad93eb477ed75bd5f4652d5a79983f00bb48cc
isabellaoliiveiiraa <bellacsoliveira123@gmail.com>
2025-08-06 19:07:51 -0300

    pag recipe_list pronta

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,18 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.home, name='recipes_home'),
-]
-
-# recipes/urls.py
-from django.urls import path # Importa a função 'path' que define URLs.
-from . import views # Importa o arquivo views.py do próprio app.
-
-urlpatterns = [
-path('', views.recipe_list, name='recipe_list'), # nova view para listar
-]
-
-urlpatterns = [
-    path('', views.recipe_list, name='recipe_list'), # view para listar receitas
-    path('<int:pk>/', views.recipe_detail, name='recipe_detail'), # view para detalhes
+    path('', views.recipe_list, name='recipe_list'),
+    # Esta linha é crucial: define a URL e nomeia ela como 'recipe_detail'
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    # Outras URLs do seu aplicativo...
 ]
